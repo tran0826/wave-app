5eb84cb047deb36a799ff65c75e53a4659ed7937
Suzuki Kaito <sephirothkaito0826@yahoo.co.jp>
2022-06-12 23:45:31 +0900

    index page css update

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,12 @@
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import {
+  LineIcon,
+  LineShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share"
 
 const name = "tran0826"
 export const siteTitle = "Wave Composite Game"
@@ -27,49 +33,58 @@ export default function Layout({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@tran0826" />
       </Head>
-      <footer className="">
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className=""
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className="">{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className=""
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className="">
-              <Link href="/">
-                <a className="">{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </footer>
       <main>{children}</main>
-      {!home && (
-        <div className="">
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
+      <div className="pt-4 bg-white sm:pt-10 lg:pt-12">
+        <footer className="px-4 mx-auto max-w-screen-2xl md:px-8">
+          <div className="flex flex-col gap-4 justify-between items-center py-6 border-y md:flex-row">
+            <nav className="flex flex-wrap gap-x-4 gap-y-2 justify-center md:gap-6 md:justify-start">
+              <Link href="/">
+                <a className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100">
+                  Top
+                </a>
+              </Link>
+              <Link href="/free">
+                <a className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100">
+                  Free
+                </a>
+              </Link>
+              <Link href="#">
+                <a className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100">
+                  About
+                </a>
+              </Link>
+            </nav>
+
+            <div className="flex gap-4">
+              <TwitterShareButton
+                url="https://wave-app-nine.vercel.app/"
+                title="Let's Play Wave composite game!"
+              >
+                <TwitterIcon size={30} round={false} />
+              </TwitterShareButton>
+              <LineShareButton
+                url="https://wave-app-nine.vercel.app/"
+                title="Let's Play Wave composite game!"
+              >
+                <LineIcon size={30} round={false} />
+              </LineShareButton>
+            </div>
+          </div>
+
+          <div className="py-8 text-sm text-center text-gray-400">
+            If you find a bug, please contact{" "}
+            <a
+              href="https://twitter.com/tran0826"
+              className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @tran0826
+            </a>
+            .
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,44 +32,58 @@ const Home: NextPage<Props> = ({ stages }) => {
       <Head>
         <title>wave composite game</title>
       </Head>
-      <section>
-        <h1>Wave Composite Game</h1>
-        <div>
-          <Link href="/free">
-            <a className="font-bold text-black">Free Mode</a>
-          </Link>
-          <ul className="p-2 font-bold text-white bg-red-500">
-            {stages.map((stage, id) => (
-              <li key={id}>
-                <Link href={`/stage/${stage.id}`}>
-                  <a>Stage {stage.id}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <ul className="">
-            {stages.map((stage, id) => (
-              <li key={id}>
-                <Link href={`/ranking/${stage.id}`}>
-                  <a>Ranking Stage {stage.id}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+      <section className="flex flex-col items-center">
+        <div className="flex flex-col items-center pt-8 pb-16 max-w-xl text-center lg:pt-32 lg:pb-48">
+          <p className="mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl">
+            Interesting simulator
+          </p>
+
+          <h1 className="mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-5xl">
+            Wave Composite Game
+          </h1>
+
+          <div className="flex flex-col gap-2.5 w-6/12 sm:justify-center">
+            <details className="text-red-500 active:text-red-700 bg-gray-200 hover:bg-gray-300 rounded-lg outline-none">
+              <summary className="block py-3 text-sm font-semibold text-center outline-none focus-visible:ring ring-indigo-300 transition duration-100 md:text-base">
+                <div>Game Mode</div>
+              </summary>
+              <ul className="">
+                {stages.map((stage, id) => (
+                  <li key={id}>
+                    <Link href={`/stage/${stage.id}`}>
+                      <a className="hover:text-indigo-500 active:text-indigo-600 transition duration-100">
+                        Stage {stage.id}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+            <details className="text-gray-500 active:text-gray-700 bg-gray-200 hover:bg-gray-300 rounded-lg outline-none">
+              <summary className="block py-3 text-sm font-semibold text-center outline-none focus-visible:ring ring-indigo-300 transition duration-100 md:text-base">
+                <div>Ranking</div>
+              </summary>
+              <ul className="">
+                {stages.map((stage, id) => (
+                  <li key={id}>
+                    <Link href={`/ranking/${stage.id}`}>
+                      <a className="hover:text-indigo-500 active:text-indigo-600 transition duration-100">
+                        Ranking Stage {stage.id}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+
+            <Link href="/free">
+              <a className="inline-block py-3 text-sm font-semibold text-center text-white bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 rounded-lg outline-none focus-visible:ring ring-indigo-300 transition duration-100 md:text-base">
+                Free Mode
+              </a>
+            </Link>
+          </div>
         </div>
       </section>
-      <TwitterShareButton
-        url="https://wave-app-nine.vercel.app/"
-        title="Let's Play Wave composite game!"
-      >
-        <TwitterIcon size={30} round={false} />
-      </TwitterShareButton>
-      <LineShareButton
-        url="https://wave-app-nine.vercel.app/"
-        title="Let's Play Wave composite game!"
-      >
-        <LineIcon size={30} round={false} />
-      </LineShareButton>
     </Layout>
   )
 }
