0d5b2e58b165e81cce46ddbe649cdaeb403d5e9c
Suzuki Kaito <sephirothkaito0826@yahoo.co.jp>
2022-06-14 05:24:00 +0900

    adsense

---
+++ components/adsense.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+
+const Adsense = () => {
+  const { asPath } = useRouter()
+
+  useEffect(() => {
+    try {
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+    } catch (err) {
+      console.log(err)
+    }
+  }, [asPath])
+
+  return (
+    <div key={asPath}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-layout="auto"
+        data-ad-format="xxx"
+        data-ad-client="xxx"
+        data-ad-slot="xxx"
+      />
+    </div>
+  )
+}
+export default Adsense
